5f184335b4d3efd92343eae2c0ff330db0679421
ANTÔNIO MURARA ZABALA <antonio_zabala@estudante.sesisenai.org.br>
2023-07-11 09:43:23 -0300

    ajustes andreola

--- assets/js/genero.js
+++ assets/js/genero.js
@@ -5,19 +5,6 @@ const mas = document.getElementById('mas')
   Chart.defaults.font.family = 'system-ui'
   Chart.defaults.font.weight = '300'
 
-// const labels = ['menor que 18','18-24','25-29','30-34','35-39','40-44','45-49','50-54','55-59','maior que 60']
-
-// const data = {
-//     labels,
-//     datasets: [{
-//         data: [4, 12, 9, 8, 6, 4, 2, 1, 2, 0],
-//         label: 'Masculinos naturais do Distrito Federal',
-//         backgroundColor:'red',
-//         borderColor: '#fff',
-//         borderWidth: 3
-//     }],
-// }
-
 new Chart(mas, {
   type: 'bar',
   data: {
@@ -25,7 +12,7 @@ new Chart(mas, {
     datasets: [{
       label: 'Masculinos naturais do Distrito Federal',
       data: [4, 12, 9, 8, 6, 4, 2, 1, 2, 0],
-      borderWidth: 1
+      borderWidth: 2
     }]
   },
   options: {
@@ -36,23 +23,6 @@ new Chart(mas, {
     }
   }
 });
-
-// const config = {
-//     type: 'bar',
-//     data,
-//     options: {
-//         scales: {
-//           y: {
-//             beginAtZero: true
-//           }
-//         },
-//         color: 'white',
-//         pointBackgroundColor: 'white',
-//         pointBorderColor: 'white',
-//       },
-// }
-
-// const graph = new Chart(mas, config)
 
 /* ============================== */
 
@@ -65,7 +35,7 @@ let fem = document.getElementById('fem')
       datasets: [{
         label: 'Femininos naturais do Distrito Federal',
         data: [0, 2, 2, 2, 1, 0, 0, 1, 1, 1],
-        borderWidth: 1
+        borderWidth: 2
       }]
     },
     options: {
@@ -88,7 +58,7 @@ const res_mas = document.getElementById('res_mas')
       datasets: [{
         label: 'Masculinos residentes do Distrito Federal',
         data: [2,7, 8, 12, 13, 7, 4, 4, 3,2],
-        borderWidth: 1
+        borderWidth: 2
       }]
     },
     options: {
@@ -111,7 +81,7 @@ const res_fem = document.getElementById('res_fem')
       datasets: [{
         label: 'Femininos naturais do Distrito Federal',
         data: [0,4,1,4,2,2,2,1,0,0],
-        borderWidth: 1
+        borderWidth: 2
       }]
     },
     options: {
