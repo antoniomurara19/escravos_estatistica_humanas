6f6f4d9e53c79076bacaaa5027bf651340b61cb0
antonio murara zabala <antoniomurar4@gmail.com>
2023-08-25 07:39:46 -0300

    calculo na idade

--- assets/js/idade.js
+++ assets/js/idade.js
@@ -8,7 +8,7 @@ const idade_mas = document.getElementById('idade_mas');
   new Chart(idade_mas, {
     type: 'radar',
     data: {
-      labels: ['menor de 18','18-24','25-29','30-34','35-39','40-44','45-49','50-54','55-59','maior de 60'],
+      labels: ['menor de 19','20-25','26-30','31-35','36-40','41-45','46-50','51-55','56-60','maior de 60'],
       datasets: [{
         label: '62 homens escravizados residentes do Distrito Federal',
         data: [2,7,8,12,13,7,4,4,3,2],
@@ -45,7 +45,7 @@ const idade_mas = document.getElementById('idade_mas');
   new Chart(idade_fem, {
     type: 'radar',
     data: {
-      labels: ['menor de 18','18-24','25-29','30-34','35-39','40-44','45-49','50-54','55-59','maior de 60'],
+      labels: ['menor de 19','20-25','26-30','31-35','36-40','41-45','46-50','51-55','56-60','maior de 60'],
       datasets: [{
         label: '16 mulheres escravizadas residentes do Distrito Federal',
         data: [0,4,1,4,2,2,2,1,0,0],
@@ -82,7 +82,7 @@ const idade_mas = document.getElementById('idade_mas');
   new Chart(idade_mas_, {
     type: 'radar',
     data: {
-      labels: ['menor de 18','18-24','25-29','30-34','35-39','40-44','45-49','50-54','55-59','maior de 60'],
+      labels: ['menor de 19','20-25','26-30','31-35','36-40','41-45','46-50','51-55','56-60','maior de 60'],
       datasets: [{
         label: '48 homens escravizados naturais do Distrito Federal',
         data: [4,12,9,8,6,4,2,1,2,0],
@@ -119,7 +119,7 @@ const idade_mas = document.getElementById('idade_mas');
   new Chart(idade_fem_, {
     type: 'radar',
     data: {
-      labels: ['menor de 18','18-24','25-29','30-34','35-39','40-44','45-49','50-54','55-59','maior de 60'],
+      labels: ['menor de 19','20-25','26-30','31-35','36-40','41-45','46-50','51-55','56-60','maior de 60'],
       datasets: [{
         label: '10 mulheres escravizadas residentes do Distrito Federal',
         data: [0,2,2,2,1,0,0,1,1,1],
@@ -158,4 +158,46 @@ const idade_mas = document.getElementById('idade_mas');
     c.addEventListener('click',()=>{
         side_bar.style.left = '-200vw'
     })
+
+    // CÁLCULO ================================
+
+let  botao_calcular = document.getElementById('botao_calcular')
+let  media_etnia_res = document.getElementById('media')
+let  variancia_res = document.getElementById('var')
+let  desvio_padrao_res = document.getElementById('dev')
+
+let dados = [19,19,23,23,23,23,23,23,23,27,27,27,27,27,27,27,27,33,33,33,33,33,33,33,33,33,33,33,33,43,43,43,43,43,43,43,43,43,43,43,43,43,47,47,47,47,47,47,47,53,53,53,53,57,57,57,61,61]
+let media_etnia = 0
+let variancia = 0
+let soma_variancia = 0
+let desvio_padrao = 0
+
+botao_calcular.addEventListener('click', ()=>{
+  let soma = 0
+  let contador = 0
+
+    for(i=0;i<dados.length;i++){
+      soma += Number(dados[i])
+      contador += 1
+    }
+
+    // MÉDIA
+    media_etnia = soma / contador;
+    console.log(media_etnia)
+
+    //VARIÂNCIA
+    for(i=0;i<dados.length;i++){
+      soma_variancia += Number((dados[i]-media_etnia)**2)
+    }
+    variancia = soma_variancia / soma
+    console.log(variancia)
+
+    // DESVIO PADRÃO
+    desvio_padrao = Math.sqrt(variancia)
+    console.log(desvio_padrao)
+
+    media_etnia_res.innerHTML = 'A média é : ' + media_etnia.toFixed(2)
+    variancia_res.innerHTML = 'A variância é : ' + variancia.toFixed(2)
+    desvio_padrao_res.innerHTML = 'O desvio padrão é : ' + desvio_padrao.toFixed(2)
+});
   
